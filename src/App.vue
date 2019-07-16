a8ee7a826a7d9f4c19b723402b793ec0d656e037
John Borden <bjorkbat@gmail.com>
2019-07-16 17:31:17 -0600

    Added my new components for app

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,41 @@
 
     <div class="container">
       <ShoutForm />
+
+      <div class="row">
+
+        <div class="column">
+          <NumberField
+            @numbers="numbers = $event"/>
+        </div>
+
+        <div class="column">
+          <NumberList :numbers="numbers" />
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import NumberField from './components/molecules/NumberField.vue';
+import NumberList from './components/molecules/NumberList.vue';
 import ShoutForm from './components/molecules/ShoutForm.vue';
 export default {
   name: 'app',
   components: {
+    NumberField,
+    NumberList,
     ShoutForm
+  },
+
+  data: function() {
+    return {
+      numbers: []
+    }
   }
+
 }
 </script>
 
