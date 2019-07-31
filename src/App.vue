4fd19e01d9fdcefc433461ea0125e6a75a30a785
Bjorkbat <bjorkbat@gmail.com>
2019-07-30 21:00:26 -0600

    Top level can manage an array of groups to pull numbers from now

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
 
-    <Modal />
+    <Modal
+      @groups="addGroups($event)"/>
 
     <header class="brand">
       <img alt="KAVI Logo" src="./assets/KAVI_logo_black.png"/>
@@ -16,19 +17,42 @@
         <div class="column">
           <NumberField
             @numbers="numbers = $event"/>
+        </div>
+
+        <div class="column">
+          <button class="button button-clear" data-micromodal-trigger="add-group-modal">
+            Add Group
+          </button>
+        </div>
+
+      </div>
+
+      <div class="row">
+
+        <div class="column">
           <NumberList :numbers="numbers" />
         </div>
 
         <div class="column">
-          <a href="#" data-micromodal-trigger="add-group-modal">Add Group</a>
+
+          <GroupList
+            :groups="groups"
+            :selected="groups"
+            :selectable="false"
+            :removeable="true"
+            :editable="false"
+            @un-select="removeGroup($event)"/>
+            
         </div>
 
       </div>
+
     </div>
   </div>
 </template>
 
 <script>
+import GroupList from './components/molecules/GroupList.vue';
 import Modal from './components/organisms/Modal.vue';
 import NumberField from './components/molecules/NumberField.vue';
 import NumberList from './components/molecules/NumberList.vue';
@@ -36,6 +60,7 @@ import ShoutForm from './components/molecules/ShoutForm.vue';
 export default {
   name: 'app',
   components: {
+    GroupList,
     Modal,
     NumberField,
     NumberList,
@@ -44,8 +69,43 @@ export default {
 
   data: function() {
     return {
+      groups: [],
       numbers: []
     }
+  },
+
+  methods: {
+
+    // Adds groups selected from the group selection modal to the array of
+    // groups here.  Checks for duplicates and removes them
+    addGroups: function(groups) {
+
+      let _this = this;
+      groups.forEach((group) => {
+
+        for(let i = 0; i < _this.groups.length; i ++) {
+          if (_this.groups[i].id == group.id) {
+            return;
+          }
+        }
+
+        _this.groups.push(group);
+
+      });
+
+    },
+
+    removeGroup: function(group) {
+
+      for(let i = 0; i < this.groups.length; i ++) {
+        if (this.groups[i].id == group.id) {
+          this.groups.splice(i, 1);
+          return;
+        }
+      }
+
+    }
+
   }
 
 }
